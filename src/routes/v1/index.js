a497c5addaf789da0cbbf09aa87540f994f9279b
Sahil Dhiman <194502@nith.ac.in>
2024-10-08 20:58:16 +0530

    add more routes to city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const CityController = require('../../controllers/city-controller.js');
 
 router.post('/city',CityController.create);
+router.delete('/city/:id',CityController.destroy);
+router.get('/city/:id',CityController.get);
+router.patch('/city/:id',CityController.update);
 
 module.exports = router;
