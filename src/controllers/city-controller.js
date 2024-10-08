f37c923d8688791c4f8c82fa4a4fbe0fb6b66d84
Sahil Dhiman <194502@nith.ac.in>
2024-10-08 19:57:20 +0530

    add controller files

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,90 @@
+const {CityService} = require('../services/index.js');
+
+const cityService = new CityService();
+
+const create = async (req,res)=>{
+    try {
+        const city = await cityService.createCity(req.body);
+        res.status(201).json({
+            data:city,
+            message:'city created',
+            success:true,
+            err:{}
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            data:{},
+            message:'city not created',
+            success:false,
+            err:error
+        })
+    }
+}
+
+const destroy = async (req,res)=>{
+    try {
+        const response = await cityService.deleteCity(req.params.id);
+        res.status(200).json({
+            data:response,
+            message:'Successfully city deleted',
+            success:true,
+            err:{}
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            data:{},
+            message:'city not deleted',
+            success:false,
+            err:error
+        })
+    }
+}
+
+const get = async (req,res)=>{
+    try {
+        const response = await cityService.getCity(req.params.id);
+        res.status(200).json({
+            data:response,
+            message:'Successfully fetched city ',
+            success:true,
+            err:{}
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            data:{},
+            message:'city not fetched',
+            success:false,
+            err:error
+        })
+    }
+}
+
+const update = async (req,res)=>{
+    try {
+        const response = await cityService.updateCity(req.params.id,req.body);
+        res.status(200).json({
+            data:response,
+            message:'Successfully fetched city ',
+            success:true,
+            err:{}
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            data:{},
+            message:'city not fetched',
+            success:false,
+            err:error
+        })
+    }
+}
+
+module.exports = {
+    create,
+    destroy,
+    get,
+    update
+}
--- src/services/city-services.js
+++ src/services/city-services.js
@@ -46,3 +46,6 @@ class CitySevice{
         }
     }
 }
+
+
+module.exports = CitySevice;
