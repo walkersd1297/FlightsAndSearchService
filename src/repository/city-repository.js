ad1c6f7b9125cf7d5e217bbe7e81fec612b70c3a
Sahil Dhiman <194502@nith.ac.in>
2024-10-09 16:22:17 +0530

    minor changes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -37,11 +37,14 @@ class CityRepository {
 
     async updateCity(cityId,data){
         try {
-            const city = await City.update(data,{
-                where:{
-                    id:cityId
-                }
-            });
+            // const city = await City.update(data,{
+            //     where:{
+            //         id:cityId
+            //     }
+            // });
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log("Something went wrong in repositry layer");
